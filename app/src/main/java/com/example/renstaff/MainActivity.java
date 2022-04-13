89b0f8d75e0f2289de72388527cbb2c2132c7c65
atatarkanov <gerronimoo@gmail.com>
2022-04-13 11:21:07 +0300

    Some fixes

--- app/src/main/java/com/example/renstaff/MainActivity.java
+++ app/src/main/java/com/example/renstaff/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.renstaff;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 
 import android.graphics.Color;
@@ -15,8 +20,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.example.renstaff.ui.MainFragment;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity{
     TextView textField;
@@ -26,10 +37,23 @@ public class MainActivity extends AppCompatActivity{
     final int MENU_COLOR_GREEN = 2;
     final int MENU_COLOR_BLUE = 3;
 
+    private ViewPager2 viewPager;
+    private TabLayout tabLayout;
+
+    private MainFragment mainFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager.findViewById(R.id.pager);
+        tabLayout.findViewById(R.id.tab_layout);
+
+        mainFragment = new MainFragment();
     }
+
+
+
 }
 
