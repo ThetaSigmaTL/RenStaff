ad1f227093063740b44ecf3d055add582fec0a9c
reagent <reagentoj@gmail.com>
2022-05-08 13:02:51 +0300

    Добавил экраны Login и Register, bottom navi

---
+++ app/src/main/java/com/example/renstaff/ui/settings/SettingsFragment.java
@@ -0,0 +1,66 @@
+package com.example.renstaff.ui.settings;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.renstaff.LoginActivity;
+import com.example.renstaff.R;
+import com.example.renstaff.databinding.FragmentSettingsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SettingsFragment extends Fragment {
+
+    private SettingsViewModel settingsViewModel;
+    private FragmentSettingsBinding binding;
+    private FirebaseAuth mAuth;
+    private Button signOut;
+    private Intent intent;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
+
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textProfile;
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+        signOut = root.findViewById(R.id.signOut);
+        intent = new Intent(getContext(), LoginActivity.class);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
