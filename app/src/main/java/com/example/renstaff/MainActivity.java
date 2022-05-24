e6ffb5f23a4a14a7f84cc81f967ba7a3b31cc30b
atatarkanov <gerronimoo@gmail.com>
2022-05-24 08:03:18 +0300

    navi bottom update

--- app/src/main/java/com/example/renstaff/MainActivity.java
+++ app/src/main/java/com/example/renstaff/MainActivity.java
@@ -1,44 +1,25 @@
 package com.example.renstaff;
 
-import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Lifecycle;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.renstaff.databinding.ActivityMainBinding;
-import com.example.renstaff.ui.MainFragment;
-import com.example.renstaff.ui.calculator.CalculatorFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity{
 
-    private ViewPager2 viewPager;
-    private TabLayout tabLayout;
-    private Intent intent;
-    private BottomNavigationView bottomNavigationView;
     ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        activityMainBinding= activityMainBinding.inflate(getLayoutInflater());
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(activityMainBinding.getRoot());
+
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
--- app/src/main/java/com/example/renstaff/ui/MainFragment.java
+++ app/src/main/java/com/example/renstaff/ui/MainFragment.java
@@ -1,40 +1,35 @@
 package com.example.renstaff.ui;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Lifecycle;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TableLayout;
-
-import com.example.renstaff.ArchiveFragment;
 import com.example.renstaff.FragmentAdapter;
-import com.example.renstaff.MainActivity;
 import com.example.renstaff.R;
-import com.example.renstaff.StatsFragment;
-import com.example.renstaff.ui.calculator.CalculatorFragment;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainFragment extends Fragment {
 
     TabLayout tabLayout;
     ViewPager2 viewPager;
+    FragmentAdapter fragmentAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.main_menu_fragment, container, false);
-        return view;
 
+
+        return view;
 
     }
 
@@ -45,17 +40,15 @@ public class MainFragment extends Fragment {
         tabLayout = view.findViewById(R.id.tab_layout);
         viewPager = view.findViewById(R.id.pager);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentAdapter fragmentAdapter = new FragmentAdapter(fragmentManager, getLifecycle());
-
+        FragmentManager fragmentManager = getChildFragmentManager();
+        fragmentAdapter = new FragmentAdapter(fragmentManager,this.getLifecycle());
         viewPager.setAdapter(fragmentAdapter);
-        tabLayout.addOnTabSelectedListener((TabLayout.BaseOnTabSelectedListener) this);
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback(){
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
-            public void onPageSelected(int position) {
-                tabLayout.selectTab(tabLayout.getTabAt(position));
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+
             }
         });
+        tabLayoutMediator.attach();
     }
-
 }
